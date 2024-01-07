9b1a8f372ee3800b1f44822fe3d643ee6afbee79
l-kaushik <contact.lokeshkaushik@gmail.com>
2024-01-07 15:05:43 +0530

    Added move semantics 1. Added move constructor and move assignement operator.

--- c++/Node.h
+++ c++/Node.h
@@ -11,15 +11,21 @@ private:
     Node<T> *m_next{nullptr};
 
 public:
-    // constructors and destructors
+    // constructors
     Node();
     Node(T value);
     Node(Node<T> *next);
     Node(T value, Node<T> *next);
 
-    // copy contructor, copy assignment operator and destructor
+    // copy contructor, copy assignment operator
     Node(const Node<T> &other);
     Node<T> &operator=(const Node<T> &other);
+
+    //move constructor, move assignment operator
+    Node(Node<T> &&other);
+    Node<T> &operator=(Node<T> &&other);
+
+    //destructor
     ~Node();
 
     // Getters
@@ -36,7 +42,7 @@ public:
 
 //<--------- METHOD DEFINITIONS --------->
 
-// constructors and destructors
+// constructors
 template <typename T>
 Node<T>::Node() : m_value(), m_next(nullptr) {}
 
@@ -50,13 +56,14 @@ template <typename T>
 Node<T>::Node(T value, Node<T> *next)
     : m_value(value), m_next(next) {}
 
-// copy contructor, copy assignment operator and destructor
+// copy contructor 
 template <typename T>
 Node<T>::Node(const Node<T> &other)
 {
     *this = other;
 }
 
+//copy assignment operator
 template <typename T>
 Node<T>& Node<T>::operator=(const Node<T> &other)
 {
@@ -72,6 +79,34 @@ Node<T>& Node<T>::operator=(const Node<T> &other)
     return *this;
 }
 
+//move constructor
+template <typename T>
+Node<T>::Node(Node<T> &&other)
+{
+    *this = std::move(other);
+}
+
+//move assignment operator
+template <typename T>
+Node<T>& Node<T>::operator=(Node<T> &&other)
+{
+    if(this == &other)
+        return *this;
+    
+    m_value = std::move(other.m_value);
+
+    delete m_next;
+    m_next = nullptr;
+    m_next = std::move(other.m_next);
+
+    //resetting temporary object
+    other.m_value = T{};
+    other.m_next = nullptr;
+    
+    return *this;
+}
+
+//destructor
 template <typename T>
 Node<T>::~Node()
 {
