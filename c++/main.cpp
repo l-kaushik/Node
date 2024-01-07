82a14df76c8704da76526992720a5173100cb540
l-kaushik <contact.lokeshkaushik@gmail.com>
2024-01-07 15:04:46 +0530

    move semantics test

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,7 +1,11 @@
 #include "Node.h"
+#include <string>
 
 int main()
 {
-    Node<int> a(4);
-    a.display();
+    Node<std::string> b("my",new Node<std::string>("Name", new Node<std::string>("Pika")));
+    Node<std::string> a ("hello", new Node<std::string>("world"));
+    b = std::move(a);
+    b.display();
+    b.getNextNode()->display();
 }
